f29b74ec594bbdb39627269340efeca26ac25369
Ishika Pandey <rahi7102005@gmail.com>
2026-09-06 14:37:00 +0530

    Solved: Current submission

--- CodeStudio/Current_submission.cpp
+++ CodeStudio/Current_submission.cpp
@@ -1,17 +1,9 @@
-#include <bits/stdc++.h>
- 
-vector<vector<int>> pairSum(vector<int> &arr, int s){
-vector<vector<int>> ans;
-for(int i=0;i<arr.size();i++){
-   for(int j=i+1;j<arr.size();j++){
-      if(arr[i]+arr[j]==s){
-         vector<int> temp;
-         temp.push_back(min(arr[i],arr[j]));
-         temp.push_back(max(arr[i],arr[j]));
-         ans.push_back(temp);
+int linearSearch(int *arr, int n, int x)
+{
+  for(int i=0;i<n;i++){
+      if(arr[i]==x){
+          return i;
       }
-   }
-}
-   sort(ans.begin(),ans.end());
-   return ans;
+  }  
+  return -1;
 }
