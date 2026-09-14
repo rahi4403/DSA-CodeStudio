096ae921788baceaed8a7c0235ecc4afac82ec37
Ishika Pandey <rahi7102005@gmail.com>
2026-09-14 11:50:55 +0530

    Solved: LCA of three Nodes

--- CodeStudio/LCA_of_three_Nodes.cpp
+++ CodeStudio/LCA_of_three_Nodes.cpp
@@ -1,23 +1,17 @@
-#include <bits/stdc++.h>
- 
-Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
-{
-    // Base cases must come first
-    if(first == NULL)
-        return second;
- 
-    if(second == NULL)
-        return first;
- 
-    // Make sure first starts with the smaller value
-    if(first->data > second->data)
-    {
-        return sortTwoLists(second, first);
+#include <bits/stdc++.h> 
+void Solve(stack<int>& s,int x){
+    if(s.empty()){ //check if stack is already empty,insert element directly
+        s.push(x);
+        return;
     }
- 
-    Node<int>* curr1 = first;
-    Node<int>* next1 = curr1->next;
-    Node<int>* curr2 = second;
-    Node<int>* next2 = curr2->next;
-if(next1 == NULL)
+    int num=s.top(); //if not empty,empty it one by one from the top,insert element at bottom
+    s.pop();
+    Solve(s,x);
+    s.push(num); //place the elements at top again
+}
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
 {
+Solve(myStack,x);
+return myStack;
+}
+ 
