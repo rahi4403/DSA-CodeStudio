218e803072e16b00e0a3a6315c480a10c06c8d11
Ishika Pandey <rahi7102005@gmail.com>
2026-09-14 16:08:02 +0530

    Solved: 84. Largest Rectangle in Histogram

--- CodeStudio/84_Largest_Rectangle_in_Histogram.cpp
+++ CodeStudio/84_Largest_Rectangle_in_Histogram.cpp
@@ -1,6 +1,20 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
-        
+    private:
+vector<int> nextSmallerElement(vector<int> arr,int n){
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    for(int i=n-1;i>=0;i--){
+        int curr=arr[i];
+        while(s.top()!=-1 && arr[s.top()]>=curr){
+            s.pop();
+        }
+        ans[i]=s.top();
+        s.push(i);
     }
-};
+    return ans;
+}
+ private:
+vector<int> prevSmallerElement(vector<int> arr,int n){
+    stack<int> s;
+    s.push(-1);
