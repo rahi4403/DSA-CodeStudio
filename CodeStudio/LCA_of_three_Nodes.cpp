a89f11aee0becf24d8815fc435627c17da7f51a9
Ishika Pandey <rahi7102005@gmail.com>
2026-09-14 12:45:01 +0530

    Solved: LCA of three Nodes

--- CodeStudio/LCA_of_three_Nodes.cpp
+++ CodeStudio/LCA_of_three_Nodes.cpp
@@ -1,17 +1,19 @@
-#include <bits/stdc++.h> 
-void Solve(stack<int>& s,int x){
-    if(s.empty()){ //check if stack is already empty,insert element directly
-        s.push(x);
+void insertAtBottom(stack <int> &s,int element){
+    if(s.empty()){
+        s.push(element);
         return;
     }
-    int num=s.top(); //if not empty,empty it one by one from the top,insert element at bottom
-    s.pop();
-    Solve(s,x);
-    s.push(num); //place the elements at top again
+int num=s.top();
+s.pop();
+insertAtBottom(s,element);
+s.push(num);
 }
-stack<int> pushAtBottom(stack<int>& myStack, int x) 
-{
-Solve(myStack,x);
-return myStack;
+void reverseStack(stack<int> &stack) {
+if(stack.empty()){
+    return;
 }
- 
+int num=stack.top();
+stack.pop();
+reverseStack(stack);
+insertAtBottom(stack,num);
+}
